4f8dd38f4fc98067e5d0bdc00422e037ad6ff559
kaichaosun <kaichaosuna@gmail.com>
2023-05-15 18:26:37 +0800

    register and delete referral

--- parachain/pallets/referral/src/lib.rs
+++ parachain/pallets/referral/src/lib.rs
@@ -17,6 +17,9 @@ pub mod pallet {
 		/// The maximum length of a metadata string.
 		#[pallet::constant]
 		type StringLimit: Get<u32>;
+		/// The maximu length of the referral code.
+		#[pallet::constant]
+		type MaxCodeLength: Get<u32>;
 		/// The runtime event
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 	}
@@ -36,10 +39,24 @@ pub mod pallet {
 	#[pallet::storage]
 	pub type Campaigns<T: Config> = StorageMap<_, Blake2_128Concat, u32, Campaign<T>>;
 
+	#[pallet::storage]
+	pub type Referrals<T: Config> = StorageDoubleMap<
+		_,
+		Blake2_128Concat,
+		u32,
+		Blake2_128Concat,
+		BoundedVec<u8, T::MaxCodeLength>,
+		T::AccountId,
+	>;
+
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
 		CampaignCreated(T::AccountId, u32),
+		CampaignUpdated(T::AccountId, u32),
+		CampaignDeleted(T::AccountId, u32),
+		ReferralRegistered(T::AccountId, u32, BoundedVec<u8, T::MaxCodeLength>),
+		ReferralDeleted(T::AccountId, u32, BoundedVec<u8, T::MaxCodeLength>),
 	}
 
 	#[pallet::error]
@@ -47,6 +64,9 @@ pub mod pallet {
 		CampaignAlreadyExists,
 		CampaignNotExists,
 		NotCampaignOwner,
+		ReferralAlreadyExists,
+		NotReferralOwner,
+		ReferralNotExist,
 	}
 
 	#[pallet::hooks]
@@ -68,7 +88,6 @@ pub mod pallet {
 			Campaigns::<T>::insert(id, campaign);
 
 			Self::deposit_event(Event::CampaignCreated(sender, id));
-
 			Ok(().into())
 		}
 
@@ -86,6 +105,7 @@ pub mod pallet {
 				ensure!(campaign.owner == sender, Error::<T>::NotCampaignOwner);
 				campaign.metadata = metadata;
 
+				Self::deposit_event(Event::CampaignUpdated(sender, id));
 				Ok(().into())
 			})
 		}
@@ -100,8 +120,48 @@ pub mod pallet {
 				ensure!(campaign.owner == sender, Error::<T>::NotCampaignOwner);
 				*maybe_campaign = None;
 
+				Self::deposit_event(Event::CampaignDeleted(sender, id));
 				Ok(().into())
 			})
+		}
+
+		#[pallet::call_index(3)]
+		#[pallet::weight(0)]
+		pub fn register_referral(
+			origin: OriginFor<T>,
+			id: u32,
+			code: BoundedVec<u8, T::MaxCodeLength>,
+		) -> DispatchResultWithPostInfo {
+			let sender = ensure_signed(origin)?;
+
+			ensure!(Campaigns::<T>::contains_key(id), Error::<T>::CampaignNotExists);
+			ensure!(!Referrals::<T>::contains_key(id, &code), Error::<T>::ReferralAlreadyExists);
+
+			Referrals::<T>::insert(id, &code, &sender);
+
+			Self::deposit_event(Event::ReferralRegistered(sender, id, code));
+			Ok(().into())
+		}
+
+		#[pallet::call_index(4)]
+		#[pallet::weight(0)]
+		pub fn delete_referral(
+			origin: OriginFor<T>,
+			id: u32,
+			code: BoundedVec<u8, T::MaxCodeLength>,
+		) -> DispatchResultWithPostInfo {
+			let sender = ensure_signed(origin)?;
+			ensure!(Campaigns::<T>::contains_key(id), Error::<T>::CampaignNotExists);
+
+			match Referrals::<T>::get(id, &code) {
+				Some(account) => ensure!(account == sender, Error::<T>::NotReferralOwner),
+				None => return Err(Error::<T>::ReferralNotExist.into()),
+			};
+
+			Referrals::<T>::remove(id, &code);
+
+			Self::deposit_event(Event::ReferralDeleted(sender, id, code));
+			Ok(().into())
 		}
 	}
 }
--- parachain/pallets/referral/src/tests.rs
+++ parachain/pallets/referral/src/tests.rs
@@ -101,3 +101,159 @@ fn delete_campaign_failed_when_not_owner() {
 		);
 	});
 }
+
+#[test]
+fn create_referral_works() {
+	new_test_ext().execute_with(|| {
+		let metadata = BoundedVec::try_from(vec![0, 1]).unwrap();
+		let referral_code = BoundedVec::try_from(vec![68, 69]).unwrap(); // code: hi
+		let campaign_owner = 1;
+		let referral_account = 2;
+
+		let _ = Referral::create_campaign(RuntimeOrigin::signed(campaign_owner), 1, metadata);
+
+		assert_ok!(Referral::register_referral(
+			RuntimeOrigin::signed(referral_account),
+			1,
+			referral_code.clone()
+		));
+
+		assert_eq!(Referrals::<Test>::get(1, &referral_code).unwrap(), 2);
+	});
+}
+
+#[test]
+fn create_referral_failed_when_campaign_not_exists() {
+	new_test_ext().execute_with(|| {
+		let referral_code = BoundedVec::try_from(vec![68, 69]).unwrap(); // code: hi
+		let referral_account = 2;
+
+		assert_noop!(
+			Referral::register_referral(RuntimeOrigin::signed(referral_account), 1, referral_code),
+			Error::<Test>::CampaignNotExists,
+		);
+	});
+}
+
+#[test]
+fn create_referral_failed_when_referral_code_already_exists() {
+	new_test_ext().execute_with(|| {
+		let metadata = BoundedVec::try_from(vec![0, 1]).unwrap();
+		let referral_code = BoundedVec::try_from(vec![68, 69]).unwrap(); // code: hi
+		let campaign_owner = 1;
+		let referral_account = 2;
+		let campaign_id = 1;
+
+		let _ =
+			Referral::create_campaign(RuntimeOrigin::signed(campaign_owner), campaign_id, metadata);
+
+		let _ = Referral::register_referral(
+			RuntimeOrigin::signed(referral_account),
+			campaign_id,
+			referral_code.clone(),
+		);
+
+		assert_noop!(
+			Referral::register_referral(
+				RuntimeOrigin::signed(referral_account),
+				campaign_id,
+				referral_code
+			),
+			Error::<Test>::ReferralAlreadyExists,
+		);
+	});
+}
+
+#[test]
+fn delete_referral_works() {
+	new_test_ext().execute_with(|| {
+		let metadata = BoundedVec::try_from(vec![0, 1]).unwrap();
+		let referral_code = BoundedVec::try_from(vec![68, 69]).unwrap(); // code: hi
+		let campaign_owner = 1;
+		let referral_account = 2;
+		let campaign_id = 1;
+
+		let _ =
+			Referral::create_campaign(RuntimeOrigin::signed(campaign_owner), campaign_id, metadata);
+
+		let _ = Referral::register_referral(
+			RuntimeOrigin::signed(referral_account),
+			campaign_id,
+			referral_code.clone(),
+		);
+
+		assert_ok!(Referral::delete_referral(
+			RuntimeOrigin::signed(referral_account),
+			campaign_id,
+			referral_code.clone()
+		));
+
+		assert_eq!(Referrals::<Test>::get(1, &referral_code), None);
+	});
+}
+
+#[test]
+fn delete_referral_failed_when_campaign_not_exists() {
+	new_test_ext().execute_with(|| {
+		let referral_code = BoundedVec::try_from(vec![68, 69]).unwrap(); // code: hi
+		let referral_account = 2;
+
+		assert_noop!(
+			Referral::delete_referral(RuntimeOrigin::signed(referral_account), 1, referral_code),
+			Error::<Test>::CampaignNotExists,
+		);
+	});
+}
+
+#[test]
+fn delete_referral_failed_when_referral_not_exists() {
+	new_test_ext().execute_with(|| {
+		let metadata = BoundedVec::try_from(vec![0, 1]).unwrap();
+		let referral_code = BoundedVec::try_from(vec![68, 69]).unwrap(); // code: hi
+		let campaign_owner = 1;
+		let referral_account = 2;
+		let campaign_id = 1;
+
+		let _ =
+			Referral::create_campaign(RuntimeOrigin::signed(campaign_owner), campaign_id, metadata);
+
+		assert_noop!(
+			Referral::delete_referral(
+				RuntimeOrigin::signed(referral_account),
+				campaign_id,
+				referral_code
+			),
+			Error::<Test>::ReferralNotExist,
+		);
+	});
+}
+
+#[test]
+fn delete_referral_failed_when_not_referral_owner() {
+	new_test_ext().execute_with(|| {
+		let metadata = BoundedVec::try_from(vec![0, 1]).unwrap();
+		let referral_code = BoundedVec::try_from(vec![68, 69]).unwrap(); // code: hi
+		let campaign_owner = 1;
+		let referral_account = 2;
+		let invalid_referral_account = 3;
+		let campaign_id = 1;
+
+		let _ =
+			Referral::create_campaign(RuntimeOrigin::signed(campaign_owner), campaign_id, metadata);
+
+		let _ = Referral::register_referral(
+			RuntimeOrigin::signed(referral_account),
+			campaign_id,
+			referral_code.clone(),
+		);
+
+		assert_noop!(
+			Referral::delete_referral(
+				RuntimeOrigin::signed(invalid_referral_account),
+				campaign_id,
+				referral_code
+			),
+			Error::<Test>::NotReferralOwner,
+		);
+	});
+}
